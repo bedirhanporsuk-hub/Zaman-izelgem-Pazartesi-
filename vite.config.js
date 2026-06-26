65362ab2a9e366ee2fd41245cd7a0ca0f5f0f46f
bedirhanporsuk-hub <bedirhanporsuk@gmail.com>
2026-06-26 22:36:16 +0300

    vite config yazim hatasi duzeltildi

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './' '/Zaman-izelgem-Pazartesi-/', // ./ yerine bunu yaz
+  base: '/Zaman-izelgem-Pazartesi-/',
   plugins: [react()],
 })
