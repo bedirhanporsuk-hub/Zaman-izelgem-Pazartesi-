d16861a31f0214307dda4f692ddc6b2369f12d67
bedirhanporsuk-hub <bedirhanporsuk@gmail.com>
2026-06-26 20:57:22 +0300

    base kaldirildi

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Zaman-izelgem-Pazartesi-/', // ./ yerine bunu yaz
   plugins: [react()],
 })
